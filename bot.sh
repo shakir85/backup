620c73b1364d9e0cde539be0d0a93c82295f42e5
shakir85 <sralmasaari@gmail.com>
2021-11-21 15:57:40 +0000

    2021-11-20 23:00:03,066 - INFO - Backup completed.

--- bot.sh
+++ bot.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-info="Backup - $(tail -1 /var/log/syncdir/syncdir.log)"
+backup="$(tail -1 /var/log/syncdir/syncdir.log)"
 
 echo "$info" >> info.txt
 echo "$info"
 echo
 
 git add info.txt
-git commit -m "$info"
+git commit -m "$backup"
 git push origin master
 
-cd - || true
+cd -
